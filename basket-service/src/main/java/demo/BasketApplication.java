16a834d4f7c0c29953a42f9b9e32daaf3aebe037
sbo21698 <sybboland@gmail.com>
2017-05-28 17:41:41 +0200

    CS1: WIP Two modules with both the greeting GET request

--- basket-service/src/main/java/demo/BasketApplication.java
+++ basket-service/src/main/java/demo/BasketApplication.java
@@ -1,12 +1,13 @@
-package hello;
+package demo;
+
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
-
+public class BasketApplication {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(BasketApplication.class, args);
     }
+
 }
