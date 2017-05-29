0ebb0fc0f28a80e8aaf8ab28669287c1b4de7d3d
sybren <sybren.boland@kvk.nl>
2017-05-29 11:52:49 +0200

    CS9: Running edge service docker. Had to implement eureka discovery service.

--- inventory-service/src/main/java/demo/InventoryApplication.java
+++ inventory-service/src/main/java/demo/InventoryApplication.java
@@ -2,14 +2,12 @@ package demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
-@EnableNeo4jRepositories
-@EnableConfigurationProperties
-@EnableTransactionManagement
+@EnableDiscoveryClient
+@EnableHystrix
 public class InventoryApplication {
     public static void main(String[] args) {
         SpringApplication.run(InventoryApplication.class, args);
